d608d87e38e9b30270f425d73a4e03e2cdcf755f
Ian <ianwachira612@outlook.com>
2022-10-06 05:04:35 -0400

    php variables

--- part9.php
+++ part9.php
@@ -1,3 +1,9 @@
 <?php
     ini_set('display_errors', 1);       // Show errors for debugging
 ?>
+
+$firstName = "Jane";       
+$lastName = "Doe";
+$age = 35;
+$married = true;
+$childrenNames = array("Alice", "Bob");
