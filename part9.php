94813663c6b06499a10b760d70a3823aecf95591
Ian <ianwachira612@outlook.com>
2022-10-06 05:09:10 -0400

    operators

--- part9.php
+++ part9.php
@@ -2,12 +2,16 @@
     ini_set('display_errors', 1);          // Show errors for debugging
     
     
-    
     $firstName = "Jane";                        // String type variable 
     $lastName = "Doe";                          // String type variable 
     $age = 35;                                  // Integer type variable 
     $married = true;                            // Boolean type variable 
     $childrenNames = array("Alice", "Bob");     // Array type variable 
+
+    $fullName = $firstName . " " . $lastName;
+    $dogYears = $age/7;
+    $isAdult = $dogYears > 4;
+    $isSettled = $isAdult and $maarried;
 ?>
 
 
