88ab08c3a652129eaec915bee8e0581bab11652b
Ian <ianwachira612@outlook.com>
2022-10-06 04:39:20 -0400

    header php code

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+      <div class="container">
+        <h1>Welcome to my Blog!</h1>
+        <p>Here is where you will find all of my posts</p>
+        <link rel="stylesheet" href="styles.css">  
+      </div>
+    </header>
+  </article>
+  <nav>
+    <a href="index.php">Index page</a> |
+    <a href="post-submission.php">Post Submission page</a> |
+    <a href="post.php">Posts</a> 
+  </nav>
