6276280589a9f9f2c3329ce45cbb58058aea40e4
Ian <ianwachira612@outlook.com>
2022-10-06 04:52:14 -0400

    nav php

--- header.php
+++ header.php
@@ -7,8 +7,3 @@
       </div>
     </header>
   </article>
-  <nav>
-    <a href="index.php">Index page</a> |
-    <a href="post-submission.php">Post Submission page</a> |
-    <a href="post.php">Posts</a> 
-  </nav>
---
+++ nav.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="index.php">Index page</a> |
+    <a href="post-submission.php">Post Submission page</a> |
+    <a href="post.php">Posts</a> 
+  </nav>
