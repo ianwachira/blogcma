24b4b84e7b3b095a93eb9056f482dc629139a63f
Ian <ianwachira612@outlook.com>
2022-10-06 05:06:45 -0400

    variables and comments

--- part9.php
+++ part9.php
@@ -1,9 +1,13 @@
 <?php
-    ini_set('display_errors', 1);       // Show errors for debugging
+    ini_set('display_errors', 1);          // Show errors for debugging
+    
+    
+    
+    $firstName = "Jane";                        // String type variable 
+    $lastName = "Doe";                          // String type variable 
+    $age = 35;                                  // Integer type variable 
+    $married = true;                            // Boolean type variable 
+    $childrenNames = array("Alice", "Bob");     // Array type variable 
 ?>
 
-$firstName = "Jane";       
-$lastName = "Doe";
-$age = 35;
-$married = true;
-$childrenNames = array("Alice", "Bob");
+
