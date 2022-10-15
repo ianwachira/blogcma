48f1d4cd6fe4db6801f5511989919db6e28943d4
Ian <ianwachira612@outlook.com>
2022-10-15 07:41:48 -0400

    dbName = blogcms

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
     $dbServer = "localhost";
     $dbUsername = "root";    // This is the same username as phpMyAdmin
     $dbPassword = "root";    // This is the same password as phpMyAdmin
-    $dbName = "blog_cms";
+    $dbName = "blogcms";
 
     // Create connection
     $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
