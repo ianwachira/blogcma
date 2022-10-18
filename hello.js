c18b0c88f3b05165883139f6e904e1efa9f9a7d2
Ian <ianwachira612@outlook.com>
2022-10-18 19:49:23 -0400

    4.6 conditional statements

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
 document.write("<h1>Hello World</h1>") 
+
+var firstName = "Jane";                     // String type variable
+var lastName = "Doe";                       // String type variable
+var age = 35;                               // Number type variable
+var married = true;                         // Boolean type variable
+var childrenNames = ["Alice", "Bob"];       // Array type variable
+var husband = { firstName: "John", lastName: "Doe", age: 35 }    
+            
+if (isSettled) {
+    document.write(fullName + " is settled.");
+} else {
+    document.write(fullName + " is not settled.");
+}
