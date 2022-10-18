a4215131ffac50c45cefd9d68fa79f13910c4920
Ian <ianwachira612@outlook.com>
2022-10-18 13:36:50 -0400

    mailto footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container">
       <p>Author: Ian Wachira</p>
-      <p><a href="iwachira@lesley.edu">iwachira@lesley.edu</a></p>
+      <p><a href="mailto:iwachira@lesley.edu">iwachira@lesley.edu</a></p>
     </div>
   </footer>
