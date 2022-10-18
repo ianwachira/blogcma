b32a3f5591aee917fb3451e926de0205ecd827f4
Ian <ianwachira612@outlook.com>
2022-10-18 19:44:55 -0400

    4.3 operators

--- hello.js
+++ hello.js
@@ -6,3 +6,8 @@ var age = 35;                               // Number type variable
 var married = true;                         // Boolean type variable
 var childrenNames = ["Alice", "Bob"];       // Array type variable
 var husband = { firstName: "John", lastName: "Doe", age: 35 }    
+
+var fullName = firstName + " " + lastName;        // Concatenation operator
+var dogYears = age/7;                             // Arithmetic Operator
+var isAdult = dogYears > 4;                       // Comparison operator
+var isSettled = isAdult && married;               // Integer type variable
