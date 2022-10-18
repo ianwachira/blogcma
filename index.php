ee75c72bf3bac974aab0c8081412cbd99ca2e496
Ian <ianwachira612@outlook.com>
2022-10-18 19:38:43 -0400

    Hello World js

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
     include 'header.php';
     include 'nav.php';
 ?>
+<script>
+    alert("Hello World");
+</script>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
