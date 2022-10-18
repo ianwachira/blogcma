ca798e0d5474da68d64eed9cb93b64c925a8e7a6
Ian <ianwachira612@outlook.com>
2022-10-18 19:47:22 -0400

    4.5 document write

--- hello.js
+++ hello.js
@@ -1,12 +1,1 @@
-alert("Hello World");
-
-var firstName = "Jane";                     // String type variable
-var lastName = "Doe";                       // String type variable
-var age = 35;                               // Number type variable
-var married = true;                         // Boolean type variable
-var childrenNames = ["Alice", "Bob"];       // Array type variable
-var husband = { firstName: "John", lastName: "Doe", age: 35 }    
-
-function getFullName(firstName, lastName) {     // Function name
-    return  firstName + " " + lastName;         // Returned value
-}
+document.write("<h1>Hello World</h1>") 
